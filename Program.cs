f55b0372a610e021e59da3e11a28955694ff1780
Deepali <96642348+deepalisagar851@users.noreply.github.com>
2022-04-08 15:19:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Display(int n)
+        {int sum=0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum = sum +i;
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("The Sum Of Numbers is :"+sum);
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Input the Limit of N numbers");
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+            Display(n);
+
+
+        }
+    }
+}
